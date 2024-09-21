1de43d82e8421c2d5caa947b0adafa09c738871e
Gilles Patrick Ngondo <37463219+gillespatrick@users.noreply.github.com>
2024-09-21 10:09:14 -0400

    Update scenario1.tf

--- terraform/aws/scenario1/scenario1.tf
+++ terraform/aws/scenario1/scenario1.tf
@@ -1,11 +1,10 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            # optional
-            version = "~> 3.0"
-       }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.68.0"
     }
+  }
 }
 
 # Configuring provider
